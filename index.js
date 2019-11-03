c432bf155077d1aae9d4fa63c88c509bc7186b29
buonzz <buonzz@gmail.com>
2019-11-04 04:35:05 +0800

    run sample script

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+const axios = require('axios')
+
 // load the environment settings 
 require('dotenv').config()
 
+axios.post('https://api.pdf.co/v1/pdf/convert/to/json', {
+    url: 'https://github.com/buonzz/pdfco-demo/raw/master/wordpress-pdf-invoice-plugin-sample.pdf',
+    inline: true
+  },{
+      headers : { "x-api-key": process.env.API_KEY}
+  })
+.then(function (response) {
+    console.log(response.data.body.document);
+  })
+.catch(function (error) {
+    console.log(error);
+  });
 
